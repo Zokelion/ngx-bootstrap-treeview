b3019960b06de302232936c72079f6d532e85b92
Zozoleclown <Zozoleclown1@gmail.com>
2019-06-14 00:56:52 +0200

    :bulb: Added "Using mapper" example
    The demo page "using mapper" is now fully functionnal

--- src/app/using-mapper/using-mapper.component.ts
+++ src/app/using-mapper/using-mapper.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { skillsByCategories, Skill, Category } from '../skills-by-categories';
+import { TreeMap, LeafMap, NgxBootstrapTreeviewMapper, Leaf, LeafClickedEvent } from 'src/lib/public_api';
+import { ILoggingService } from 'src/lib/interfaces/ILoggingService.interface';
 
 @Component({
-  selector: 'app-using-mapper',
-  templateUrl: './using-mapper.component.html',
-  styleUrls: ['./using-mapper.component.scss']
+    selector: 'app-using-mapper',
+    templateUrl: './using-mapper.component.html',
+    styleUrls: ['./using-mapper.component.scss']
 })
 export class UsingMapperComponent implements OnInit {
+    public logger: ILoggingService = console;
+    public data = skillsByCategories;
+    public selectedLeaves: Leaf[] = [];
 
-  constructor() { }
+    public treeMap: TreeMap = {
+        children: 'children',
+        leavesKey: 'skills',
+        value: 'id',
+        label: 'name'
+    };
 
-  ngOnInit() {
-  }
+    public leafMap: LeafMap = {
+        value: 'id',
+        label: 'label'
+    };
 
+    public mapper = new NgxBootstrapTreeviewMapper<Category, Skill>(this.treeMap, this.leafMap);
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    public onBranchClicked(event) {
+        console.log(event);
+    }
+
+    public onLeafClicked(event: LeafClickedEvent) {
+        console.log(event);
+        this.selectedLeaves = event.selectedLeaves;
+    }
 }
